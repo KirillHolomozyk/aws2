dfd4450f73f5b0b90f0dfabc0e49b539003b970d
KirillHolomozyk <94526599+KirillHolomozyk@users.noreply.github.com>
2023-12-26 23:19:59 +0100

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  } 
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aus_security_group" "web_app" {
+  name = "web_app"
+  description = "security group"
+  ingress {
+    grom_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  ingress {
+    grom_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  engress {
+    grom_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "web_app"
+  }
+}
+
+resource "aws_instance" "webapp_instance" {
+  ami = "ami-0669b163befffbdfc"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "webapp_instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.webapp_instance.public_ip
+  sevsitive = true
+}
